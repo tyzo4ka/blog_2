854835f71f686e74faf152d58134b6211fa96c28
tyzo4ka <tyzo4ka@gmail.com>
2019-10-21 14:47:12 +0600

    Added tags in ArticleForm  in forms.py

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -3,6 +3,8 @@ from .models import Article, Comment
 
 
 class ArticleForm(forms.ModelForm):
+    tags = forms.CharField(max_length=20, required=False, label='Тэги')
+
     class Meta:
         model = Article
         exclude = ['created_at', 'updated_at']
